dafad92abfcc2ea1e2565ecc4125a3b266fa8c63
Heather Floyd <heatherflo@gmail.com>
2023-11-29 13:33:09 -0700

    starting style set

---
+++ app.js
@@ -0,0 +1,65 @@
+const locations = [
+  '🏤', '🏥', '🏭', '🏢', '🏣', '⛽️',
+]
+
+const people = [{
+  name: 'Jimbo',
+  picture: '🤵',
+  location: '🏤'
+},
+{
+  name: 'Sammy',
+  picture: '🙆‍♀️',
+  location: '⛽️'
+},
+{
+  name: 'Michael',
+  picture: '👷',
+  location: '🏤'
+},
+{
+  name: 'Robert',
+  picture: '👷',
+  location: '🏥'
+},
+{
+  name: 'Terry',
+  picture: '🤴',
+  location: '🏥',
+},
+{
+  name: 'Bill',
+  picture: '🕵️',
+  location: '⛽️',
+},
+{
+  name: 'Marie',
+  picture: '👩‍🍳',
+  location: '🏭',
+},
+{
+  name: 'Mykeal',
+  picture: '💂',
+  location: '🏭',
+},
+{
+  name: 'Phil',
+  picture: '🧜‍♂️',
+  location: '🏭',
+},
+{
+  name: 'Wilson',
+  picture: '🏐',
+  location: '🏢',
+},
+{
+  name: 'Wendy',
+  picture: '👩‍⚕️',
+  location: '⛽️',
+},
+{
+  name: 'Jeremy',
+  picture: '🦹',
+  location: '🏢',
+}
+]
